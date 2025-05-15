6bd1ae8a7193cb22623defc5d7e0748c9113f3f3
Almouhannad Hafez <almouhannad.hafez@gmail.com>
2025-05-15 23:48:48 +0300

    Regoster todo bloc and use cases in DI

--- lib/injection.dart
+++ lib/injection.dart
@@ -2,19 +2,36 @@ import 'package:get_it/get_it.dart';
 import 'package:todo_list_app/data/datasources/todo_local_datasource.dart';
 import 'package:todo_list_app/data/repositories/todo_repository_impl.dart';
 import 'package:todo_list_app/domain/repositories/todo_repository.dart';
+import 'package:todo_list_app/domain/usecases/add_todo.dart';
+import 'package:todo_list_app/domain/usecases/delete_todo.dart';
+import 'package:todo_list_app/domain/usecases/fetch_todos.dart';
+import 'package:todo_list_app/domain/usecases/update_todo.dart';
+import 'package:todo_list_app/presentation/bloc/todo_bloc.dart';
 
 final sl = GetIt.instance;
 
 Future<void> initInjection() async {
-  // Data sources
+  // Data sources & repositories (as before) …
   sl.registerLazySingleton<TodoLocalDataSource>(
     () => TodoLocalDataSourceImpl(),
   );
-
-  // Repositories
   sl.registerLazySingleton<TodoRepository>(
     () => TodoRepositoryImpl(localDataSource: sl()),
   );
 
-  // You can add other services (use cases, Blocs, etc.) here later
+  // Use cases
+  sl.registerLazySingleton(() => FetchTodos(sl()));
+  sl.registerLazySingleton(() => AddTodo(sl()));
+  sl.registerLazySingleton(() => UpdateTodo(sl()));
+  sl.registerLazySingleton(() => DeleteTodo(sl()));
+
+  // Bloc
+  sl.registerFactory(
+    () => TodoBloc(
+      fetchTodos: sl(),
+      addTodo: sl(),
+      updateTodo: sl(),
+      deleteTodo: sl(),
+    ),
+  );
 }
